91f20ee0db441e75861ea6be34c1219748eb8fb7
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-04-18 18:15:03 -0300

    Autenticação: Pemitindo o login do usuário

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { is } = require('express/lib/request');
+require('dotenv').config();
+const secret = process.env.JWT_TOKEN;
+
 
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
@@ -14,4 +19,26 @@ router.post('/register', async (req, res) => {
   }
 });
 
-module.exports = router;
+router.post('/login', async(req, res) => {
+  const { email, password} = req.body;
+
+  try {
+    let user = await User.findOne({email});
+    if(!user)
+    res.status(401).json({error: 'Incorrect email or password.'});
+    else {
+      user.isCorrectPassword(password, function(err, same) {
+        if(!same)
+        res.status(401).json({error: 'Incorrect email or password.'});
+        else {
+          const token = jwt.sign({email}, secret, { expiresIn: '10d' });
+          res.json({user: user, token: token});
+        }
+      })
+    }
+  } catch (error) {
+    res.status(500).json({error: 'Internal error, please try again.'});
+  }
+})
+
+module.exports = router; jjjjj
