a99a15e07ba1b87e72c557590f6d4543802b09e8
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-05-12 22:48:25 -0300

    Navegação básica

--- javascript_note/src/assets/components/Header/index.js
+++ javascript_note/src/assets/components/Header/index.js
@@ -1,39 +1,56 @@
 import React from "react";
-import { Navbar, Container } from "rbx";
+import { Navbar, Container, Column } from "rbx";
 import logoImage from "../../images/logo.png";
 import "../../styles/header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
 	return (
 		<Navbar>
 			<Container>
 				<Navbar.Brand>
-					     <img src={logoImage} alt="Logo" />     
+					<Link to="/">
+						     <img src={logoImage} alt="Logo" />   
+					</Link>
 					<Navbar.Burger
 						className="navbar-burger burger"
 						aria-label="menu"
 						aria-expanded="false"
 						data-target="navbar-menu"
 					>
-						       <span aria-hidden="true"></span>       
+						<span aria-hidden="true"></span>       
 						<span aria-hidden="true"></span>       
 						<span aria-hidden="true"></span>     
-					</Navbar.Burger>
+					</Navbar.Burger>{" "}
 					   
 				</Navbar.Brand>
-				   
 				<Navbar.Menu id="navbar-menu">
-					     
 					<Navbar.Segment
 						as="div"
 						className="navbar-item navbar-end"
 						align="right"
 					>
-						      Item 1      
-					</Navbar.Segment>{" "}
-					   
-				</Navbar.Menu>{" "}
-				 
+						<Column.Group>
+							<Column>
+								<Link
+									to="/register"
+									className="button is-white has-text-custom-purple"
+								>
+									Register
+								</Link>
+							</Column>
+							<Column>
+								<Link
+									to="/login"
+									className="button is-outlined is-custom-purple"
+								>
+									Login
+								</Link>
+							</Column>
+						</Column.Group>
+					</Navbar.Segment>
+				</Navbar.Menu>
+				  
 			</Container>
 		</Navbar>
 	);
--- javascript_note/src/routes.js
+++ javascript_note/src/routes.js
@@ -1,14 +1,34 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	useRoutes,
-} from "react-router-dom";
-import Home from "./assets/screens/home";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
+import HomeScreen from "./assets/screens/home";
+import RegisterScreen from "./assets/screens/auth/register";
+import LoginScreen from "./assets/screens/auth/login";
+import NotesScreen from "./assets/screens/notes/index";
+import UsersEditScreen from "./assets/screens/users/edit";
+import PrivateRoute from "./assets/screens/auth/private_router";
 
 const App = () => {
-	let routes = useRoutes([{ path: "/", element: <Home /> }]);
+	let routes = useRoutes([
+		{ path: "/", element: <HomeScreen /> },
+		{ path: "/register", element: <RegisterScreen /> },
+		{ path: "/login", element: <LoginScreen /> },
+		{
+			path: "/notes",
+			element: (
+				<PrivateRoute>
+					<NotesScreen />
+				</PrivateRoute>
+			),
+		},
+		{
+			path: "/users/edit",
+			element: (
+				<PrivateRoute>
+					<UsersEditScreen />
+				</PrivateRoute>
+			),
+		},
+	]);
 	return routes;
 };
 
